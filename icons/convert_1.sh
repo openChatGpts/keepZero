e969f5b3ae7bd758de79672600d6e6913c6e1b86
sanbo <sanbo.xyz@gmail.com>
2024-09-06 17:50:57 +0800

    update sth. at 2024-09-06 17:50:57

--- icons/convert_1.sh
+++ icons/convert_1.sh
@@ -1,3 +1,7 @@
 convert icon.webp -resize 16x16 icon-16.png
 convert icon.webp -resize 48x48 icon-48.png
 convert icon.webp -resize 128x128 icon-128.png
+
+magick icon.webp -resize 16x16 icon-16.png
+magick icon.webp -resize 48x48 icon-48.png
+magick icon.webp -resize 128x128 icon-128.png
